974025c7e3c7e25d1239d2ea90ca210852d0520a
ayazshukla <75552457+ayazshukla@users.noreply.github.com>
2021-04-25 21:57:39 -0400

    Update b1.cpp

--- 2021/b1.cpp
+++ 2021/b1.cpp
@@ -4,6 +4,7 @@
 #define  sort(v) sort(v.begin(),v.end());
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
 #define in(t) cin>>t;
+#define line() cout << "\n";
 using namespace std; typedef  long  long  ll; typedef vector<int> vi; typedef set<int> si;
 int main(){
  ios::sync_with_stdio(0);cin.tie(0); int t; in(t);
@@ -11,13 +12,12 @@ int main(){
 	 int n; in(n); int cnt=1; string s; in(s);
 	 cout << "Case #" << i+1 << ": 1 ";
 	 for(int j=0;j<n-1;++j){
-		 if(s[j+1]>s[j]){
-			 ++cnt; cout << cnt << " ";
-		 }
-		 else {
-			 cnt=1; cout << cnt << " ";
-		 }
+		 if(s[j+1]>s[j])
+			 ++cnt;
+		 else
+			 cnt=1;
+		 cout << cnt << " ";
 	 }
-	 cout << endl;
-  }
+	 line();
+ }
 }
